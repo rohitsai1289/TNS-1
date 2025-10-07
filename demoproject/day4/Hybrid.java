fcf7b2d1120bba1681790d36d1910fee0f130b2a
rohitsai1289 <rohitsai8008@gmail.com>
2025-10-07 15:42:38 +0530

    inheritance

---
+++ demoproject/day4/Hybrid.java
@@ -0,0 +1,46 @@
+package com.tnsif.day4;
+
+public class Hybrid {
+	public static void main(String[] args) {
+		 StudentAthlete sa = new StudentAthlete();
+		 sa.displayPerson();
+		 sa.displayStudent();
+        sa.displaySports();
+	     sa.displaySkills();
+        sa.displayAthlete();
+	}
+}
+
+class Person {
+   void displayPerson() {
+       System.out.println("I am a person.");
+   }
+}
+
+class Student5 extends Person {
+   void displayStudent() {
+       System.out.println("I am a student.");
+   }
+}
+
+interface Sports {
+   void displaySports();
+}
+
+interface Skills {
+   void displaySkills();
+}
+
+class StudentAthlete extends Student5 implements Sports , Skills {
+   public void displaySports() {
+       System.out.println("I play football.");
+ }
+   
+   public void displaySkills() {
+       System.out.println("I have leadership skills.");
+   }
+   
+   void displayAthlete() {
+       System.out.println("I am a Student Athlete.");
+   }
+}
